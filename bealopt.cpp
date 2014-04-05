2fd92c3b39ab68afe9f5a3b95b568130ae2aa06f
fwjmath <fwjmath@gmail.com>
2014-04-05 19:32:31 +0200

    First algorithm - New modulus
    A highly composite modulus is used to optimize hitting rate of hash
    table.

--- bealopt.cpp
+++ bealopt.cpp
@@ -7,21 +7,22 @@
 
 #define BASE_MAX 1000
 #define EXPO_MAX 1000
-#define MODP1(X) ((X) % 4294967291ULL)
-#define MODP2(X) ((X) % 4294967279ULL)
+//#define MODP1(X) ((X) % 3491888400ULL) //highly composite number
+#define MODP1(X) ((X) % 4227022800ULL) //highly abundant number
+#define MODP2(X) ((X) % 4294967087ULL) //safe prime
 #define YIELD(X,A,Y,B) printf("%u ^ %u + %u ^ %u\n", X, A, Y, B);
 
 using namespace std;
 
 unordered_set< unsigned long long > p1hash, p2hash;
 
-unsigned long long h1cnt, h2cnt, candcnt;
+unsigned long long h1cnt, h2cnt;
 
 void build_hash(){
 	p1hash.clear();
 	p2hash.clear();
 	for(unsigned int z=1; z<=BASE_MAX; z++){
-		unsigned long long zp1=MODP1(MODP1(z*z)*z), zp2=MODP2(MODP2(z*z)*z);
+		unsigned long long zp1=MODP1(MODP1((unsigned long long)z*z)*z), zp2=MODP2(MODP2((unsigned long long)z*z)*z);
 		p1hash.insert(zp1);
 		p2hash.insert(zp2);
 		for(unsigned int c=4; c<=EXPO_MAX; c++){
@@ -36,7 +37,7 @@ void build_hash(){
 }
 
 void init(){
-	h1cnt=0; h2cnt=0; candcnt=0;
+	h1cnt=0; h2cnt=0;
 	build_hash();
 	return;
 }
@@ -78,15 +79,14 @@ unsigned long long fastexpP2(unsigned int base, unsigned int expo){
 void search(){
 	unsigned long long xp1, xp2, yp1, yp2;
 	for(unsigned int x=2; x < BASE_MAX; x++){
-		xp1=MODP1(x*x);
+		xp1=MODP1((unsigned long long)x*x);
 		for(unsigned int a=3; a <= EXPO_MAX; a++){
 			xp1=MODP1(xp1*x);
 			for(unsigned int y=x+1; y <= BASE_MAX; y++){
 				if(ullgcd(x,y)!=1) continue;
-				yp1=MODP1(y*y);
+				yp1=MODP1((unsigned long long)y*y);
 				for(unsigned int b=3; b <= EXPO_MAX; b++){
 					yp1=MODP1(yp1*y);
-					candcnt++;
 					if(p1hash.find(MODP1(xp1+yp1))!=p1hash.end()){
 						h1cnt++;
 						xp2=fastexpP2(x,a);
@@ -106,6 +106,7 @@ void search(){
 int main(){
 	init();
 	search();
-	printf("Candidates: %llu\nHash1: %llu\nHash2: %llu\n", candcnt, h1cnt, h2cnt);
+	printf("Hash1: %llu\nHash2: %llu\n", h1cnt, h2cnt);
+	printf("Size1: %llu\nSize2: %llu\n", p1hash.size(), p2hash.size());
 	return 0;
 }
